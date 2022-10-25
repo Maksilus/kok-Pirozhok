b05c2e8f2fe99312a89cc603574c1f52595430e1
Maksilus <mvasyukov1@gmail.com>
2022-10-25 20:59:18 +0300

    Вариант 4.	Дан файл целых чисел, найти количество квадратов нечётных чисел.

--- main.py
+++ main.py
@@ -13,4 +13,25 @@ try:
         if x=="":
             break
 except(ValueError):
-    print(c)
+    print("Количество квадратов нечётных чисел: ",c)
+orda.close()
+orda=open("orda.txt",'r')
+cCH=0
+cN=0
+cO=0
+try:
+    while True is True:
+        a=int((orda.readline()))
+        b=a%2
+        if a==0:
+            cO+=1
+        elif b==1 and a!=0:
+            cN+=1
+        else:
+            cCH+=1
+        if x=="":
+            break
+except(ValueError):
+    print("Количество нечётных чисел: ", cN)
+    print("Количество чётных чисел: ",cCH)
+    print("Количество нулей: ", cO)
